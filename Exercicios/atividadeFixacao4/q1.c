b30c4b9d3eb0bc69b0ec572649d29d4eb2db858a
saul-rocha <saul.rocha2001@gmail.com>
2021-04-14 10:52:39 -0300

    simulação do algoritmo

--- Exercicios/atividadeFixacao4/q1.c
+++ Exercicios/atividadeFixacao4/q1.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-//nro = 5   i = 0     bin=[]
+/*
+// 1° nro = 5   i = 0    bin[8] |  2° nro = 2 i = 0 bin[8] | 3° nro = 1 i = 0 bin[8]
 void binario(int nro, int *i,char bin[8])
 {
     if (nro < 256)
     {
         if(nro / 2 > 0)
         {
-            binario(nro/2,i,bin);   // 1º nro=2 | 2º nro=0
-            bin[*i] = (nro % 2) + 48; //pendencia -> bin[0] = 5%2=1+48 = '1' | bin[1] = 2%2=0+48 = '0'
-            printf("bin[%d] = %c\n",*i,bin[*i]); //bin[0] = 1 | bin[0] = 0
-            (*i)++; // i=1 | i = 2
+            binario(nro/2,i,bin);  // 1° | 2°
+            bin[*i] = (nro % 2) + 48; //pendencia desempilha -> nro = 2, bin[1]= 0 + 48 = '0' | nro = 5, bin[0] = 1 +  48 = '1'
+            printf("bin[%d] = %c\n",*i,bin[*i]); //pendencia bin[1] = '0' | bin[2] = '1'
+            (*i)++; //pendencia  i = 2
         }
-        else { bin[*i] = (nro % 2) + 48; // 3º nro = 0 -> nro%2 =0+48 = '1'
-            printf("bin[%d] = %c\n",*i,bin[*i]);
-            (*i)++;
+        // na terceira vez da recursão vem para esta condição e depois faz as operações pendentes acima
+        else { bin[*i] = (nro % 2) + 48; // 3º bin[0] = 1 % 2 = 1 + 48 = '1'
+            printf("bin[%d] = %c\n",*i,bin[*i]); //3º bin[0] = '1'
+            (*i)++; //3º i = 1
         }
     }
 }
@@ -34,3 +35,6 @@ int main()
     else printf("O maior número possível com 8 dígitos é 255. \n");
     return(0);
 }
+*/
+
+// MODIFICAÇÃO
