d29da6d7d84828263bb13af6cf9fb3dda323b1cf
Saul Rocha <55061070+saul-rocha@users.noreply.github.com>
2021-09-02 14:53:18 -0300

    comentarios q2 e q4

--- Exercicios EDII/atividade_fixaxão1/q2.c
+++ Exercicios EDII/atividade_fixaxão1/q2.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+#define MAX 50
 //Faça uma função que dado um número inteiro 
 //devolva os divisores do mesmo, a função 
 //deve ser recursiva.
 
-void retorna_divisores(int vet[],int n, int aux, int *tamanho){
-    if(aux > 0){
+///tem como parametros o vetor do tipo inteiro onde será colocado os divisores,
+/// o valor inteiro a ser retirado os divisores, aux inicia com o mesmo valor de n, 
+/// tamanho é passado por referencia para pegar o tamanho do vetor
+void retorna_divisores(int vet[MAX],int n, int aux, int *tamanho){
+    if(aux > 0){//condição de parada
         if(n % aux == 0){
-            vet[*tamanho] = aux;
-            *tamanho = *tamanho + 1;
+            vet[*tamanho] = aux;//atribui o valor para o vetor se for divisor
+            *tamanho = *tamanho + 1;//tamanho é incrementado
         }
-        retorna_divisores(vet,n,--aux,tamanho);
+        retorna_divisores(vet,n,--aux,tamanho);//recursão sempendencia decrementando o aux
     }
 }
 
-void imprimir(int vetor[10], int index, int tamanho_vetor){
-    if(index < tamanho_vetor){ 
+//imprime o vetor de divisores
+// recebe o vetor com os divisores, o index inicia de 0 para percorrer o vetor 
+// e o tamanho do vetor com conteúdo
+void imprimir(int vetor[MAX], int index, int tamanho_vetor){
+    if(index < tamanho_vetor){ //condição de parada
         printf("vetor[%d] = %d\n",index,vetor[index]); 
-        imprimir(vetor,++index,tamanho_vetor);
+        imprimir(vetor,++index,tamanho_vetor);//recursão sem pendencia incrementando o index
     }
 }
 
@@ -27,7 +33,7 @@ void imprimir(int vetor[10], int index, int tamanho_vetor){
 int main(){
 
     int vet[50];
-    int n = 10;
+    int n = 50;
     int aux = n;
     int *tamanho = (int *)malloc(sizeof(int));
     *tamanho = 0;
--- Exercicios EDII/atividade_fixaxão1/q4.c
+++ Exercicios EDII/atividade_fixaxão1/q4.c
@@ -8,10 +8,13 @@ lidos.
 (b) Repita o exercício da letra a, mas agora faça uma função recursiva que deixa pendência.
 */
 
+//recebe os 5 valores e um auxiliar que incia com 2 e 
+//um ponteiro MMC que ja começa como sendo 1
+// aux inicia de 2 pois 1 já é o MMC
 void MMC_sem_pendencia(int a,int b,int c,int d,int e,int aux, int *MMC){
     int flag = 0;
-    int numero;
-
+    int numero; 
+    //se os valores forem diferente de 1 e o numero for divisivel por auz a flag recebe 1
     if(a != 1 && a % aux == 0){
         a = a/aux;
         flag = 1;
@@ -33,27 +36,31 @@ void MMC_sem_pendencia(int a,int b,int c,int d,int e,int aux, int *MMC){
         flag = 1;
     }
 
+    //quando a flag for 1 o MMC é atrubuido se a soma dos valores for igual a 5
     if( a+b+c+d+e == 5 && flag == 1){
-        *MMC = (*MMC) *aux;
+        *MMC = (*MMC) * aux;
     }else{
+        //caso contrario, se somente a flag for 1   , numero recebe a aux
         if(flag == 1){
             numero = aux;
+        //SSe flag for 0, numero recebe 1 e o MMC continua o mesmo
         }else{
             numero = 1;
             aux++;
         }
         //printf("Depois\na = %d\nb = %d\nc = %d\nd = %d\ne = %d\naux = %d\nnumero = %d\nMMC = %d\n\n",a,b,c,d,e,aux,numero,*MMC);
         *MMC = (*MMC) * numero;
-        MMC_sem_pendencia(a,b,c,d,e,aux,MMC); 
+        MMC_sem_pendencia(a,b,c,d,e,aux,MMC);//chama recusivamente sem pendencia até que a flag seja 1 
     }
 
 }
 
+//idem anterior, muda apenas o parametro MMC que agora é retornado pela função e não por parametro
 int MMC_com_pendencia(int a,int b,int c,int d,int e, int aux){
     int flag = 0;
     int MMC = 1;
     int numero;
-    int numero_2;
+    int numero_2;// é usado para receber o valor retornado na função
 
     if(a != 1 && a % aux == 0){
         a = a/aux;
@@ -75,7 +82,7 @@ int MMC_com_pendencia(int a,int b,int c,int d,int e, int aux){
         e = e/aux;
         flag = 1;
     }
-
+    
     if( a+b+c+d+e == 5){
         MMC = aux;
     }else{
@@ -86,8 +93,8 @@ int MMC_com_pendencia(int a,int b,int c,int d,int e, int aux){
             aux++;
         }
         //printf("Depois\na = %d\nb = %d\nc = %d\nd = %d\ne = %d\naux = %d\nnumero = %d\n\n",a,b,c,d,e,aux,numero);
-        numero_2 = MMC_com_pendencia(a,b,c,d,e,aux);
-        MMC = MMC * numero * numero_2; 
+        numero_2 = MMC_com_pendencia(a,b,c,d,e,aux);//recursão com pendencia
+        MMC = MMC * numero * numero_2; //pendecia
     }
 
 
